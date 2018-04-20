42254c5bfc4b88ef9ac02eaf6943d9ff5ddd68c5
LuoFan <fanluo@amazon.com>
2018-04-20 10:32:42 +0800

    change picture to local

--- newAnnualReport/pages/1/1.js
+++ newAnnualReport/pages/1/1.js
@@ -1,3 +1,4 @@
+const app = getApp()
 Page({
   data: {
     motto: 'Hello World1'
@@ -6,5 +7,10 @@ Page({
     wx.navigateTo({
       url: '../2/2'
     });
-  }
+  },
+   onLoad: function () {
+    app.AppMusic.seek(16);
+    app.AppMusic.src = 'http://mp3.qqmusic.cc/yq/205387371.mp3';
+    // http://mp3.qqmusic.cc/yq/205387371.mp3
+   }
 })
--- newAnnualReport/pages/index/index.js
+++ newAnnualReport/pages/index/index.js
@@ -14,12 +14,13 @@ Page(
     });
   },
   onLoad: function () {
-    app.AppMusic.seek(16);
-    app.AppMusic.src = 'http://mp3.qqmusic.cc/yq/205387371.mp3';
+    //app.AppMusic.seek(16);
+    //app.AppMusic.src = '';
+    // http://mp3.qqmusic.cc/yq/205387371.mp3
     if (app.globalData.userInfo) {
       this.setData({
         userInfo: app.globalData.userInfo,
-        hasUserInfo: true
+        hasUserInfo: false
       })
     } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
@@ -27,7 +28,7 @@ Page(
       app.userInfoReadyCallback = res => {
         this.setData({
           userInfo: res.userInfo,
-          hasUserInfo: true
+          hasUserInfo: false
         })
       }
     } else {
@@ -37,7 +38,7 @@ Page(
           app.globalData.userInfo = res.userInfo
           this.setData({
             userInfo: res.userInfo,
-            hasUserInfo: true
+            hasUserInfo: false
           })
         }
       })
